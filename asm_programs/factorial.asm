cd5f40dfd9229fc9e34b25c256197419b26e6acf
Vitia <berbenets.vd@phystech.edu>
2023-01-12 13:15:01 +0300

    factorial by recursia

--- asm_programs/factorial.asm
+++ asm_programs/factorial.asm
@@ -1,6 +1,8 @@
+
 text_begin
 
 push 10
+push 58
 push 108
 push 97
 push 105
@@ -12,31 +14,64 @@ push 97
 push 70
 push 10
 
-
 text_end
 
 in
 pop ax
+
 push 1
 pop bx
+
 push 1
-pop cx
-push bx
-: 1
+pop fx
+
+call 1
+
+push fx
+
 push ax
+print 
+out
+
+text_begin
+
+push 32
+push 61
+push 32
+push 33
+
+
+text_end
+
+print
+
+text_begin
+
+push 10
+
+text_end
+
+hlt
+
+: 1
+
+push bx
+push ax
+
 je : 2
+
+push fx
 push 1
 push bx
 sum
 pop bx
-push cx
 push bx
 mul
-pop cx 
-push bx
-jmp : 1
-: 2
-push cx
 
-print
-hlt
+pop fx
+
+call 1
+
+: 2
+
+ret
