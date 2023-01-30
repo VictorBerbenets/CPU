ec14ee2bd6f805e93fbfbe507af5c8876d32677f
Vitia <berbenets.vd@phystech.edu>
2023-01-30 13:12:53 +0300

    -

---
+++ asm/colors.h
@@ -0,0 +1,10 @@
+#ifndef My_colors
+#define My_colors
+
+#define Grey   "\033[0m"
+#define Red    "\033[0;31m"
+#define White  "\033[1m"
+#define Blue   "\033[0;36m"
+#define Under  "\033[4m"
+
+#endif
