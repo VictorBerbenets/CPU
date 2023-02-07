07b0bbd3b68368b9603382dc86cc420c65d6ca97
Vitia <berbenets.vd@phystech.edu>
2023-02-08 00:08:07 +0300

    -

--- cpu/DSL.h
+++ cpu/DSL.h
@@ -3,7 +3,6 @@
 
 #define Push_number()         StackPush(&my_cpu->stack_cpu, *(Data*)(my_cpu->data + *number))
 #define Push_register()       StackPush(&my_cpu->stack_cpu, my_cpu->cpu_registers[(int)my_cpu->data[*number]])
-#define Push_reg1()           StackPush(&my_cpu->stack_cpu, my_cpu->cpu_registers[(int)my_cpu->data[*number]])
 #define Push_ram_number()     StackPush(&my_cpu->stack_cpu, my_cpu->ram[*(int*)(my_cpu->data + *number)])
 #define Pop()                 StackPop(&my_cpu->stack_cpu);
 #define Pop_to_register()     my_cpu->cpu_registers[(int) my_cpu->data[*number]] = StackPop(&my_cpu->stack_cpu)
